2d17eb755ed4b585989f20936081a7882d8e5194
anvar <anvartar2002@gmail.com>
2023-10-21 14:01:16 +0300

    boilerplate commit

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+  
+import App from './app';
+  
+export default () => <App/>;
+  
+let rootElement: ReactDOM.Root
+  
+export const mount = (Component, element = document.getElementById('app')) => {
+  const rootElement = ReactDOM.createRoot(element);
+  rootElement.render(<Component/>);
+
+  if(module.hot) {
+      module.hot.accept('./app', ()=> {
+        rootElement.render(<Component/>);
+      })
+  }
+};
+
+export const unmount = () => {
+  rootElement.unmount();
+};
