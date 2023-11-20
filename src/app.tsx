2a0f205c05f5726bb84cc02234599d3e3e34da97
anvar <anvartar2002@gmail.com>
2023-11-20 16:53:58 +0300

    create basic routing for project demo

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,9 @@ import GlobalStyle from "./theme/GlobalStyle";
 import HomePage from "./pages/HomePage/HomePage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignInPage from "./pages/SignInSignUp/SignInPage/SignInPage";
+import SignUpPage from "./pages/SignInSignUp/SignUpPage/SignUpPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +15,18 @@ const router = createBrowserRouter([
     element: <HomePage />,
     errorElement: <NotFoundPage />,
   },
+  {
+    path: "/score-scout/sign-in",
+    element: <SignInPage />,
+  },
+  {
+    path: "/score-scout/sign-up",
+    element: <SignUpPage />,
+  },
+  {
+    path: "/score-scout/profile",
+    element: <ProfilePage />,
+  }
 ]);
 
 const App = (): React.JSX.Element => {
