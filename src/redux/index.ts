77c664ccb044c5213797c5a80802387bb92324b2
Anvar Iskhakov <anvartar2002@gmail.com>
2023-12-04 15:24:29 +0300

    Update src/redux/index.ts

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -3,15 +3,12 @@ import { setupListeners } from "@reduxjs/toolkit/query";
 
 import bracketReducer from "./slices/bracket/bracketSlice";
 import modalReducer from "./slices/modal/modalSlice";
-import tournamentSlice from "./slices/tournament/tournamentSlice";
 
 export const store = configureStore({
   reducer: {
-    [tournamentSlice.reducerPath]: tournamentSlice.reducer,
     bracket: bracketReducer,
     modal: modalReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tournamentSlice.middleware),
 });
 
 setupListeners(store.dispatch);
