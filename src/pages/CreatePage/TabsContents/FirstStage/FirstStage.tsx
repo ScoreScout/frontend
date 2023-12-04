e4a0620c8b62d6d4c9e1db824c4de10955567306
notoriousisk <ishkineyev@mail.ru>
2023-12-04 22:58:03 +0300

    initial ui for First stage tab

--- src/pages/CreatePage/TabsContents/FirstStage/FirstStage.tsx
+++ src/pages/CreatePage/TabsContents/FirstStage/FirstStage.tsx
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 
-const FirstStage = (): React.JSX.Element => {
-  return <div>First Stage</div>;
+import { type StageProps } from "../../../../types/createPageTabTypes";
+import { ButtonSize } from "../../../../types/buttonTypes";
+import Button from "../../../../components/Button/Button";
+
+import {
+  AddPlayersWrapper,
+  ChooseWayWrapper,
+  ChooseTitle,
+  ChooseWay,
+  ButtonText,
+  ChooseCompetitionConfigWrapper,
+  TableChooseNumber,
+  BracketSystemTypesWrapper,
+  BracketSystemButtonText,
+} from "./style";
+
+import { TbTournament } from "react-icons/tb";
+import { FaTableCells } from "react-icons/fa6";
+
+enum CompetitionOptions {
+  TABLE = "Table",
+  BRACKET = "Bracket",
+  NONE = "None",
+}
+
+const FirstStage = ({ players, numStages, ratingToggleOn }: StageProps): React.JSX.Element => {
+  const [optionChosen, setOptionChosen] = useState<CompetitionOptions>(CompetitionOptions.NONE);
+
+  const handleOptionClick = (option: CompetitionOptions): void => {
+    setOptionChosen(option);
+  };
+
+  return (
+    <AddPlayersWrapper>
+      <ChooseWayWrapper optionchosen={optionChosen}>
+        <ChooseTitle>Choose how you want to conduct competition</ChooseTitle>
+        <ChooseWay>
+          <Button
+            primary={optionChosen === CompetitionOptions.TABLE}
+            size={ButtonSize.S}
+            onClick={() => {
+              handleOptionClick(CompetitionOptions.TABLE);
+            }}
+          >
+            <ButtonText>
+              <FaTableCells />
+              Table
+            </ButtonText>
+          </Button>
+          <Button
+            primary={optionChosen === CompetitionOptions.BRACKET}
+            size={ButtonSize.S}
+            onClick={() => {
+              handleOptionClick(CompetitionOptions.BRACKET);
+            }}
+          >
+            <ButtonText>
+              <TbTournament />
+              Bracket
+            </ButtonText>
+          </Button>
+        </ChooseWay>
+      </ChooseWayWrapper>
+      {optionChosen === CompetitionOptions.TABLE ? (
+        <ChooseCompetitionConfigWrapper>
+          <ChooseTitle>How many groups do you want?</ChooseTitle>
+          <TableChooseNumber type='text' />
+        </ChooseCompetitionConfigWrapper>
+      ) : optionChosen === CompetitionOptions.BRACKET ? (
+        <ChooseCompetitionConfigWrapper>
+          <ChooseTitle>Choose system for bracket</ChooseTitle>
+          <BracketSystemTypesWrapper>
+            <Button primary={true} size={ButtonSize.S}>
+              <BracketSystemButtonText>Single elimination</BracketSystemButtonText>
+            </Button>
+            <Button primary={false} size={ButtonSize.S}>
+              <BracketSystemButtonText>Double elimination</BracketSystemButtonText>
+            </Button>
+            <Button primary={false} size={ButtonSize.S}>
+              <BracketSystemButtonText>Swiss system</BracketSystemButtonText>
+            </Button>
+          </BracketSystemTypesWrapper>
+        </ChooseCompetitionConfigWrapper>
+      ) : (
+        <></>
+      )}
+    </AddPlayersWrapper>
+  );
 };
 
 export default FirstStage;
--- src/pages/CreatePage/TabsContents/FirstStage/style.tsx
+++ src/pages/CreatePage/TabsContents/FirstStage/style.tsx
@@ -1,1 +1,93 @@
 import styled from "styled-components";
+
+import { globalSemiBoldFontStyles } from "../../../../theme/FontStyles";
+
+enum CompetitionOptions {
+  TABLE = "Table",
+  BRACKET = "Bracket",
+  NONE = "None",
+}
+
+const AddPlayersWrapper = styled.div`
+  padding: 5rem 6.25rem 0 6.25rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ChooseWayWrapper = styled.div<{ optionchosen: CompetitionOptions }>`
+  display: flex;
+  flex-direction: column;
+  gap: 1.875rem;
+  margin-top: ${(props) => (props.optionchosen === CompetitionOptions.NONE ? "7.5rem" : 0)};
+`;
+
+const ChooseTitle = styled.div`
+  ${globalSemiBoldFontStyles};
+  color: ${(props) => props.theme.color.fontColor};
+`;
+
+const ChooseWay = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1.875rem;
+`;
+
+const ChooseCompetitionConfigWrapper = styled.div`
+  margin-top: 3.75rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  gap: 1.875rem;
+`;
+
+const TableChooseNumber = styled.input`
+  width: 5rem;
+  height: 2.5rem;
+  border-radius: 1rem;
+  box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.25);
+  border: 1.5px solid #aca4a2;
+  white-space: nowrap;
+  text-align: center;
+  ${globalSemiBoldFontStyles};
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0px 0px 4px 2px rgba(0, 0, 0, 0.25);
+  }
+`;
+
+const BracketSystemTypesWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1.875rem;
+`;
+
+const ButtonText = styled.div`
+  ${globalSemiBoldFontStyles};
+  font-size: 1.25rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 0.625rem;
+  width: 8rem;
+`;
+
+const BracketSystemButtonText = styled(ButtonText)`
+  width: none;
+  font-size: 0.875rem;
+  padding: 0;
+  display: inline-block;
+`;
+
+
+export {
+  AddPlayersWrapper,
+  ChooseWayWrapper,
+  ChooseTitle,
+  ChooseWay,
+  ButtonText,
+  ChooseCompetitionConfigWrapper,
+  TableChooseNumber,
+  BracketSystemTypesWrapper,
+  BracketSystemButtonText,
+};
